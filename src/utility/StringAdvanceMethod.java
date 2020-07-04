3b7c04f0acd4ffa6ed5300229f830638ae67c184
Vishnu <vishnurpillai9914@gmail.com>
2020-07-04 18:49:42 +0530

    FSJ0520A101

--- src/utility/StringAdvanceMethod.java
+++ src/utility/StringAdvanceMethod.java
@@ -1,3 +1,37 @@
 package utility;
 
 // Type your code
+public class StringAdvanceMethod {
+	String inputA, inputB;
+	char blah;
+	public String getInputA(String inputA) {
+		return this.inputA;
+	}
+	
+	public String getInputB(String inputB) {
+		return this.inputB;
+	}
+	
+	public void setInputA() {
+		this.inputA=inputA;
+	}
+	
+	public void setInputB() {
+		this.inputB=inputB;
+	}
+	
+	public String concatenate(String inputA, String inputB) {
+		return inputA.concat(inputB);
+	}
+	
+	public String[] splitter(String inputA, String inputB) {
+		return inputA.split(inputB);
+	}
+	
+	public String index(String inputA, char blah) {
+		int val=inputA.indexOf(blah);
+		String finalVal=Integer.toString(val);
+		return finalVal;
+	}
+	
+}
--- src/utility/StringMethod.java
+++ src/utility/StringMethod.java
@@ -1,3 +1,31 @@
 package utility;
 
 // Type your code
+public class StringMethod {
+	String text;
+	
+	public String lower(String text) {
+		return text.toLowerCase();
+	}
+	
+	public String upper(String text) {
+		return text.toUpperCase();
+	}
+	
+	public String first(String text) {
+		char c = text.charAt(0);
+		String s=Character.toString(c);
+		return s;
+	}
+	
+	public String len(String text) {
+		int l = text.length();
+		String length = Integer.toString(l);
+		return length;
+	}
+	
+	public String trimmer(String text) {
+		return text.trim();
+	}
+	
+}
